8244c94ec38cfdda147310d1d9050fa539a4bfa0
Your Name <2919943230@qq.com>
2019-11-25 11:36:17 +0800

    弹射之前的代码

---
+++ README.md
@@ -0,0 +1,6 @@
+# #测试
+
+## git clone项目
+
+## 使用yarn start 启动
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carouseler from "./Carousel"
+
+function App() {
+  return (
+    <div>
+        <Carouseler></Carouseler>
+    </div>
+  );
+}
+
+export default App;
